a7869620b33a26aa0c38451861e4a63f1d838715
Tirumal68 <tiru68@gmail.com>
2026-04-24 14:43:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { netlify } from "@netlify/vite-plugin-tanstack-start";
+
 export default defineConfig({
   server: {
     preset: "netlify"
+  },
+  vite: {
+    plugins: [netlify()]
   }
 });
